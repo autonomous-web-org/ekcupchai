b7e04c57a03c563ec7d33743ee64a288623d20ca
Lakshya sharma <64037374+LAKSHYA9941@users.noreply.github.com>
2025-03-07 10:38:59 +0000

    bug fix1`

--- front/ekcupchai/components/navbar.tsx
+++ front/ekcupchai/components/navbar.tsx
@@ -18,9 +18,9 @@ const Nav: React.FC = () => {
     return (
         <nav>
             <ul>
-                <li><a href="/components/about.tsx">About</a></li>
-                <li><a href="/components/home">Home</a></li>
-                <li><a href="/components/contact">Contact</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/contact">Contact</a></li>
             </ul>
         </nav>
     );
