bd822efc922db5f1397f6a347f9e986d5753d874
Lakshya sharma <64037374+LAKSHYA9941@users.noreply.github.com>
2025-03-07 13:44:58 +0000

    INDEX page composed

--- front/ekcupchai/src/App.tsx
+++ front/ekcupchai/src/App.tsx
@@ -1,37 +1,3 @@
-// <<<<<<< HEAD
-// import React from 'react'
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import './App.css'
-// import Nav from '../components/navbar'
-// import Home from '../components/home'
-// import About from '../components/about'
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/components/home",
-//     element: <div>Home Page</div>
-//   },
-//   {
-//     path: "/components/about",
-//     element: <div>About Page</div>
-//   }
-// ])
-
-// function App() {
-//   return (
-//     <>
-//       <div><h1>Fatty Buddha </h1></div>
-//       <Nav />
-//       <RouterProvider router={router} />
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -58,19 +24,3 @@ function App() {
   );
 }
   export default App;
-
-// =======
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-
-//   return (
-//     <>
-//       <h1 className='text-xl font-bold text-center'>hello world</h1>
-//     </>
-//   )
-// >>>>>>> refs/remotes/origin/Lakshya-Setup
-// }
